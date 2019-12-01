6b5ea46a4ed342bf910a8a1a63bcc3386d1f776f
Toomas Romer <toomasr@gmail.com>
2019-12-01 17:41:22 +0200

    Format source

--- src/main/java/com/toomasr/sgf4j/board/BoardSquare.java
+++ src/main/java/com/toomasr/sgf4j/board/BoardSquare.java
@@ -1,8 +1,11 @@
 package com.toomasr.sgf4j.board;
 
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
@@ -12,10 +15,14 @@ import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 
 public class BoardSquare extends StackPane {
-  private static final Image blackStoneImage = new Image(BoardSquare.class.getResourceAsStream("/stones/regular/black-stone.svg"));
-  private static final Image blackStoneHighlightedImage = new Image(BoardSquare.class.getResourceAsStream("/stones/regular/black-stone-highlighted.svg"));
-  private static final Image whiteStoneImage = new Image(BoardSquare.class.getResourceAsStream("/stones/regular/white-stone.svg"));
-  private static final Image whiteStoneHighlightedImage = new Image(BoardSquare.class.getResourceAsStream("/stones/regular/white-stone-highlighted.svg"));
+  private static final Image blackStoneImage = new Image(
+      BoardSquare.class.getResourceAsStream("/stones/regular/black-stone.svg"));
+  private static final Image blackStoneHighlightedImage = new Image(
+      BoardSquare.class.getResourceAsStream("/stones/regular/black-stone-highlighted.svg"));
+  private static final Image whiteStoneImage = new Image(
+      BoardSquare.class.getResourceAsStream("/stones/regular/white-stone.svg"));
+  private static final Image whiteStoneHighlightedImage = new Image(
+      BoardSquare.class.getResourceAsStream("/stones/regular/white-stone-highlighted.svg"));
   public static int width = 29;
 
   private static final double FONT_MULTIPLIER = 1.8125;
@@ -38,12 +45,12 @@ public class BoardSquare extends StackPane {
     this.x = x;
     this.y = y;
 
-    getStyleClass().add("board-square"); 
+    getStyleClass().add("board-square");
 
     addTheBgIntersection();
   }
 
-	private void addTheBgIntersection() {
+  private void addTheBgIntersection() {
     if (lineH != null) {
       getChildren().remove(lineH);
     }
@@ -54,13 +61,11 @@ public class BoardSquare extends StackPane {
       lineH = new Line(width / 2 + 1, width / 2, width, width / 2);
       getChildren().add(lineH);
       setAlignment(lineH, Pos.CENTER_RIGHT);
-    }
-    else if (x == 19) {
+    } else if (x == 19) {
       lineH = new Line(width / 2, width / 2, width - 1, width / 2);
       getChildren().add(lineH);
       setAlignment(lineH, Pos.CENTER_LEFT);
-    }
-    else {
+    } else {
       getChildren().add(lineH);
       setAlignment(lineH, Pos.CENTER);
     }
@@ -75,13 +80,11 @@ public class BoardSquare extends StackPane {
       lineV = new Line(width / 2, width / 2 - 1, width / 2, 0);
       getChildren().add(lineV);
       setAlignment(lineV, Pos.BOTTOM_CENTER);
-    }
-    else if (y == 19) {
+    } else if (y == 19) {
       lineV = new Line(width / 2, width / 2, width / 2, width - 1);
       getChildren().add(lineV);
       setAlignment(lineV, Pos.TOP_CENTER);
-    }
-    else {
+    } else {
       getChildren().add(lineV);
       setAlignment(lineV, Pos.CENTER);
     }
